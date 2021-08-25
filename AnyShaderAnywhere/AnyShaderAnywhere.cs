04107491e62383aa14b2823d68786530154143be
eia485 <eia485git@gmail.com>
2021-08-24 17:33:51 -0700

    Add project files.

---
+++ AnyShaderAnywhere/AnyShaderAnywhere.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using System;
+namespace AnyShaderAnywhere
+{
+    public class AnyShaderAnywhere : NeosMod
+    {
+        public override string Name => "AnyShaderAnywhere";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosAnyShaderAnywhere/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.AnyShaderAnywhere");
+            harmony.PatchAll();
+
+        }
+        [HarmonyPatch(typeof(MaterialProvider), "EnsureSharedShader")]
+        class AnyShaderAnywherePatch
+        {
+             public static bool Prefix(AssetRef<Shader> assetRef, Uri url, MaterialProvider __instance, ref IAssetProvider<Shader> __result){
+                if (assetRef.Target == null)
+                    assetRef.Target =  (IAssetProvider<Shader>)AccessTools.Method(typeof(MaterialProvider), "GetSharedShader").Invoke(__instance, new object[] { url });
+                __result = assetRef.Target;
+                return false;
+            }
+        }
+    }
+}
